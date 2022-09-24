0a4a620f8fec3812c44df81e52b22a787b0dff99
lilian <lilian.wangechi@student.moringaschool.com>
2022-09-24 13:24:21 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,53 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
+const [formData,setFormData] = useState({
+  firstName :"John",
+  lastName :"Henry"
+});
+  // function handleFirstNameChange(event){
+  //   setFirstName(event.target.value)
+  // } 
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  // function handleLastNameChange(event) {
+  //   setLastName(event.target.value);
+  // }
+// function handleFirstNameChange(event){
+//   setFormData({
+//     ...formData,firstName:event.target.value,
+//   });
+// }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+// function handleLastNameChange(event){
+//   setFormData({
+//     ...formData,
+//     lastName:event.target.value,
+//   });
+// }
 
+function handleChange(event){
+  //name is the KEY in of the formData object we are trying to update
+  const name = event.target.name;
+  const value = event.target.value;
+
+  setFormData({
+    ...formData,
+    [name]:value,
+  })
+}
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <input type="text" name="firstName" onChange={handleChange} value={formData.firstName} />
+    <input type="text" name="lastName" onChange={handleChange} value={formData.lastName} />
+    <button type="submit">Submit</button>
+  </form>
+    // <form>
+    //   <input type="text" name="firstName" onChange={handleFirstNameChange} value={formData.firstName} />
+    //   <input type="text" name="lastName" onChange={handleLastNameChange} value={formData.lastName} />
+    //   <button type="submit">Submit</button>
+    // </form>
   );
 }
 
